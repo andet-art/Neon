d21546f42441d524a9a6db2fd10fb64dc38dd6d8
gerti064 <gertirexhepi06@gmail.com>
2025-05-23 12:06:33 +0200

    changes to component files from jsx -> tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-import About from './components/About/About';
-import Cart from './components/Cart/Cart';
-import Categories from './components/Categories/Categories';
-import Contact from './components/Contact/Contact';
-import DashboardHome from './components/Dashboard/DashboardHome';
-import Header from './components/Dashboard/Header';
-import Layout from './components/Dashboard/Layout';
-import Sidebar from './components/Dashboard/Sidebar';
-import Home from './components/Home/Home';
-import Language from './components/Language/Language';
-import Profile from './components/Profile/Profile';
-import Service from './components/Service/Service';
-import Shop from './components/Shop/Shop';
+// import About from './components/About/About.tsx';
+// import Cart from './components/Cart/Cart.tsx';
+// import Categories from './components/Categories/Categories.tsx';
+// import Contact from './components/Contact/Contact.tsx';
+// import DashboardHome from './components/Dashboard/DashboardHome.tsx';
+// import Header from './components/Dashboard/Header.tsx';
+// import Layout from './components/Dashboard/Layout.tsx';
+// import Sidebar from './components/Dashboard/Sidebar.tsx';
+// import Home from './components/Home/Home.tsx';
+// import Language from './components/Language/Language.tsx'; // ✅ fixed spelling
+// import Profile from './components/Profile/Profile.tsx';
+// import Service from './components/Service/Service.tsx';
+// import Shop from './components/Shop/Shop.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
